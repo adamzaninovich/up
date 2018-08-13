218b4eb22fb0ac27f0911d3607cd6d9a47f0f208
Adam Zaninovich <adam.zaninovich@gmail.com>
2018-08-13 10:41:43 -0700

    config for heroku

--- lib/up_web/channels/user_socket.ex
+++ lib/up_web/channels/user_socket.ex
@@ -5,7 +5,7 @@ defmodule UpWeb.UserSocket do
   channel("image_set:*", UpWeb.ImageSetChannel)
 
   ## Transports
-  transport(:websocket, Phoenix.Transports.WebSocket)
+  transport(:websocket, Phoenix.Transports.WebSocket, timeout: 45_000)
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
