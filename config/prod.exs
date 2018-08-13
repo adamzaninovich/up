28b1209b21b19f22e27a8c432c00cce85957cfe0
Adam Zaninovich <adam.zaninovich@gmail.com>
2018-08-13 10:48:43 -0700

    fix erroe

--- config/prod.exs
+++ config/prod.exs
@@ -2,9 +2,9 @@ use Mix.Config
 
 config :up, UpWeb.Endpoint,
   load_from_system_env: true,
-  url: [scheme: "https", host: "agile-chamber-30906.herokuapp.com", port: 443], force_ssl: [rewrite_on: [:x_forwarded_proto]],
-
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "agile-chamber-30906.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 config :up, Up.Repo,
@@ -12,6 +12,5 @@ config :up, Up.Repo,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true
-
 
 config :logger, level: :info
